84d19c5947b5f8ae79454698b3cf0b3b2afcf143
nietbaymuratbaev0999@gmail.com <nietbaymuratbaev0999@gmail.com>
2024-11-25 22:59:07 +0500

    INFRA:For Project Car Methods

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TaskCar9
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Car car = new Car("Nexia3", 2023, 13500, 25000);
+            car.Model = "Tracker";
+            car.Year = 2024;
+            car.Price = 20000;
+            car.Speed = 7000;
+            decimal diference = car.CalculateDepreciation(5);
+            System.Console.WriteLine(car.GetCarinfo());
+            System.Console.WriteLine($"5 yildan keying narxi:{diference}");
+        }
+    }
+}
